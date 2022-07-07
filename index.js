81307fa0dee0baaf3fc95b79095629cf82448b9b
jobirbek <wwwjobirchik7@mail.ru>
2022-07-08 01:05:33 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,162 @@
+// Number
+// String
+// Boolean true false
+// undefined
+// null
+// NaN
+
+// b = "jsfksdfs5565"
+// // a = 25 / 2
+// a = 25 / 2
+// console.log( a / 2)
+
+// console.log(Number.parseInt(a / 2))
+
+// console.log(Number.parseFloat(a / 2))
+
+// console.log(a.toFixed(a / 2))
+
+// console.log(typeof b, typeof a)
+
+// console.log(typeof b, typeof a)
+
+// console.log(a.toFixed(a / 4))
+
+
+///////////////////////////////
+// math round soni yahlitlash
+// a =Math.round(3.69);  
+// console.log(a);
+
+// a =Math.round(2.33)
+// console.log(a)
+
+
+// a = 3.69
+// console.log(Math.round(a)) 
+//////////////////////////////////////// ok  
+
+/////////////////////////////////////////////////
+// Math pow-- biror sonni darajaga kotarish
+// a = Math.pow(2, 3)
+//  console.log(a)
+// a = 9
+// console.log(Math.pow(a, 2))
+
+//  a = 12
+//  console.log(Math.pow(a, 3))  
+//  console.log(Math.pow(a, 6))
+////////////////////
+//  b = 5
+// console.log(Math.pow(5, 3))  
+//////////////////////////////////////
+
+//////////////////////////////////////////////////////
+// math sqrt-ildiz ostidan chiqarish
+// a = 125
+// // b=60
+// console.log(Math.sqrt(a))
+/////////////////////////////////  okkkkk
+
+
+//////////////////////////////////////////////
+// // math cbrt - kub ildiz ostidan chiqarish
+// a = 9
+// b = 6
+// console.log(Math.cbrt(a))
+/////////////////////////////////////// OKKKKKKKKKKK
+
+/////////////////////////////////////////
+// math abs - moduldan ciqarish
+// console.log(Math.abs(a - b))
+//////////////////////////////// tushinmadim 
+
+
+///////////////////////////////////////
+// math ceil - yuqoriga yahlitlash
+// a = 3.600
+// 
+// console.log(Math.ceil(a))
+
+
+//////////////////////////////// OKAY  
+// math.floor- pastga yahlitlash
+
+// a = 3,700
+// console.log(Math.floor(a))
+// ///////////////////////////// ok
+
+
+/////////////////////////////////////////////////////
+// Math.min---berilgan sonlarni eng kichik qiymatini topish
+// a =  78,26,12,2,0,
+// console.log(Math.min(a))
+/////////////////////////////////////////////////////// OKKKAY
+
+
+////////////////////////////////////////////////////
+// math max --berilgan sonlarning kattasini topish
+// console.log(Math.max(79,102,26,12))
+// a = 78,55,33,66,10,
+// console.log(Math.max(a))
+//////////////////////////////////////////////  OKAY
+
+
+//////////////////////////////////// 
+// console.log(parseInt(Math.random()*200))
+// a = 300
+// console.log(parseInt(Math.random()*200))
+/////////////////////////////////// OKKK
+
+////////////////////////////
+// oddiy text
+// var string = "Be coder g2"
+// console.log(string.lastIndexOf("Be"))??????
+
+// console.log(string.indexOf("Be"));
+
+// console.log(string.includes("Be"))
+
+// console.log(string.search("academy"));
+
+//  console.log(string.slice(0, -2));
+
+// console.log(string.substring(0, 4))
+
+// console.log(string.substr(0 , 4))
+
+// console.log(string.replace('Be', 'become'));
+
+// ///////////////////////////////
+// var string = 'be coder academy be coder academy be somethin';
+// console.log(string.replace(/be/g,'jobir' 
+///////////////////////////////
+
+
+
+// number = "01027219403"
+letter = "Be coder academy"
+// var result = letter.toLowerCase()
+
+
+// math = "2+2 * 2"
+// math = " 2+2* 7"
+// console.log(new String(math));
+
+
+// console.log(eval(math))
+
+
+
+// console.log(result.replace(/be/g, "Becomming"))
+// console.log(letter.trim())
+//  str = "010"
+// console.log(number.padStart(number.length + 5, "*"))
+
+// console.log(str.startsWith("010"))
+// console.log(str.endsWith("0"))
+
+// console.log(letter.toUpperCase());
+console.log(letter.toLowerCase())
+
+
